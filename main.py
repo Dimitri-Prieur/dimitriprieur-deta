4813abba6efb9123e4fae4f60f90b4239e466ebd
DimitriPrieu <dimitri.prieur155@hotmail.fr>
2022-12-05 15:22:02 +0100

    Added forgotten steps & google analytics

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-def app(event):
-    return "Hello, world!"
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+
+def hello_world():
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async
+    src="https://www.googletagmanager.com/gtag/js?id=UA-251033747-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-251033747-1');
+    </script>
+    """
+    return prefix_google + "Hello World"
