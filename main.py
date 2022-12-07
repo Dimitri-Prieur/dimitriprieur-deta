6304a18c3daf5882a7bd312a1c3bd6a1493e0a1b
DimitriPrieu <dimitri.prieur155@hotmail.fr>
2022-12-07 15:19:01 +0100

    Adding log

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+import logging 
+import sys
 
 app = Flask(__name__)
+
+LOGGER = logging.getLogger(__name__)
 
 @app.route('/', methods=["GET"])
 
@@ -17,3 +21,11 @@ def hello_world():
     </script>
     """
     return prefix_google + render_template("index.html")
+
+@app.route('/logger', methods=["GET"])
+def hello_world2():
+    print('This is a back end log!', file=sys.stderr)
+    logging.info("Test logging")
+    script = """
+    <script> console.log("This is a front end log!") </script>"""
+    return "This is a test" + render_template("logger.html") + script
