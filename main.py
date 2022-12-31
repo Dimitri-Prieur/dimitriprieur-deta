be37fb04afcb9db950251e1a310a6b593de08b54
DimitriPrieu <dimitri.prieur155@hotmail.fr>
2022-12-31 16:39:09 +0100

    API connection

--- main.py
+++ main.py
@@ -3,9 +3,76 @@ import logging
 import sys
 import requests
 import os
-# from dotenv import load_dotenv
+from apiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
+
+SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
+KEY_FILE_LOCATION = 'digitaltracesapi-6e7d1ecf06d4.json'
+VIEW_ID = "281161014"
+
+def initialize_analyticsreporting():
+  """Initializes an Analytics Reporting API V4 service object.
+
+  Returns:
+    An authorized Analytics Reporting API V4 service object.
+  """
+  credentials = ServiceAccountCredentials.from_json_keyfile_name(
+      KEY_FILE_LOCATION, SCOPES)
+
+  # Build the service object.
+  analytics = build('analyticsreporting', 'v4', credentials=credentials)
+
+  return analytics
+
+
+def get_report(analytics):
+  """Queries the Analytics Reporting API V4.
+
+  Args:
+    analytics: An authorized Analytics Reporting API V4 service object.
+  Returns:
+    The Analytics Reporting API V4 response.
+  """
+  return analytics.reports().batchGet(
+      body={
+        'reportRequests': [
+        {
+          'viewId': VIEW_ID,
+          'dateRanges': [{'startDate': 'today', 'endDate': 'today'}],
+          'metrics': [{'expression': 'ga:users'}],
+          #'dimensions': [{'name': 'ga:country'}]
+        }]
+      }
+  ).execute()
+
+
+def print_response(response):
+  """Parses and prints the Analytics Reporting API V4 response.
+
+  Args:
+    response: An Analytics Reporting API V4 response.
+  """
+  for report in response.get('reports', []):
+    columnHeader = report.get('columnHeader', {})
+    dimensionHeaders = columnHeader.get('dimensions', [])
+    metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
+
+    for row in report.get('data', {}).get('rows', []):
+      dimensions = row.get('dimensions', [])
+      dateRangeValues = row.get('metrics', [])
+
+      for header, dimension in zip(dimensionHeaders, dimensions):
+        print(header + ': ', dimension)
+
+      for i, values in enumerate(dateRangeValues):
+        print('Date range:', str(i))
+        for metricHeader, value in zip(metricHeaders, values.get('values')):
+          print(metricHeader.get('name') + ':', value)
+
+
 app = Flask(__name__)
 
 LOGGER = logging.getLogger(__name__)
@@ -56,6 +123,15 @@ def get_analytics():
     r = requests.post(other_url, data=payload)
     req = requests.get(other_url, cookies=r.cookies)
     return req.text
+
+@app.route('/test', methods = ['GET', 'POST'])
+def get_test():
+
+    analytics = initialize_analyticsreporting()
+    response = get_report(analytics)
+    print(response["reports"])
+    logging.info("Test gdx")
+    return "test"
 
 if __name__ == "__main__":
     app.run(debug=True)
